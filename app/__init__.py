9a271897905ee42dce4aaae5021a5b55e824e49b
deepikagandla7456 <deepikagandla07@gmail.com>
2026-05-18 01:14:41 +0530

    Fixed Flask app factory for Vercel

--- app/__init__.py
+++ app/__init__.py
@@ -16,7 +16,7 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///skillnexora.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-    db.init_app(app)
+    # db.init_app(app)
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
     login_manager.login_message = "Please log in to continue."
@@ -36,13 +36,12 @@ def create_app():
     app.register_blueprint(streak)
     app.register_blueprint(main)
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+        # db.create_all()
 
     return app
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    from app.models import User
-    return User.query.get(int(user_id))
+    return None
